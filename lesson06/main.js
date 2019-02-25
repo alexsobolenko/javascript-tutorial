68edd7a2897f16307ed110e62ecc830c6c073176
alexsobolenko <alexsobolenko@gmail.com>
2019-02-26 02:09:12 +0300

    added lesson 06

---
+++ lesson06/main.js
@@ -0,0 +1,41 @@
+// Строки
+
+let str = 'Javascript',
+    str2 = 'ReactJS',
+    str3 = 'Vue.js';
+
+console.log('str.length');
+console.log(str.length);
+
+console.log('str + str2');
+console.log(str + str2);
+
+console.log('str.concat(str3)');
+console.log(str.concat(str3));
+
+console.log('str.charAt(3)');
+console.log(str.charAt(3));
+
+console.log('str.charCodeAt(3)');
+console.log(str.charCodeAt(3));
+
+console.log('str.substring(2, 6)');
+console.log(str.substring(2, 6));
+
+console.log('str.slice(-4)');
+console.log(str.slice(-4));
+
+console.log('str.substr(2, 6)');
+console.log(str.substr(2, 6));
+
+console.log('str3.split(".")');
+console.log(str3.split('.'));
+
+console.log('str.replace("ava", "uiu")');
+console.log(str.replace('ava', 'uiu'));
+
+console.log('str3.toUpperCase()');
+console.log(str3.toUpperCase());
+
+console.log('str2.toLowerCase()');
+console.log(str2.toLowerCase());
