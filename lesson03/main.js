dc8076953f3ff065b577fee68a29813f370164fe
alexsobolenko <alexsobolenko@gmail.com>
2019-02-26 02:07:18 +0300

    added lesson 03

---
+++ lesson03/main.js
@@ -0,0 +1,28 @@
+// Statements (инструкции), expressions (выражения), operators (операторы).
+
+// операторы
+// арифметические
+console.log(2 + 3);
+console.log(12 - 3);
+console.log(2 * 3);
+console.log(12 / 3);
+console.log(11 % 3);
+console.log(2 + 3 * 5);
+
+let i = 4, d = 4;
+console.log(++i);
+console.log(--d);
+let i2 = 4, d2 = 4;
+console.log(i2++);
+console.log(d2--);
+
+// сравнения
+let a = 4, b = 7, c = '4';
+console.log(a < b);
+console.log(a <= b);
+console.log(a >= b);
+console.log(a > b);
+console.log(a == c);
+console.log(a != c);
+console.log(a === c);
+console.log(a !== c);
