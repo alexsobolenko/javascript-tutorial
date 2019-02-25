ae4f9b56ee40d309783d5308e3f20f673a9b5c85
alexsobolenko <alexsobolenko@gmail.com>
2019-02-26 02:08:43 +0300

    added lesson 05

---
+++ lesson05/main.js
@@ -0,0 +1,41 @@
+// Числа
+let a = 234.567;
+
+console.log('toFixed(2)');
+console.log(a.toFixed(2));
+
+console.log('toExponential()');
+console.log(a.toExponential());
+
+console.log('toPrecision(4)');
+console.log(a.toPrecision(4));
+
+console.log('typeof toString()');
+console.log(typeof a.toString());
+
+console.log('Math.sqrt(a)');
+console.log(Math.sqrt(a));
+
+console.log('Math.pow(a, 3)');
+console.log(Math.pow(a, 3));
+
+console.log('Math.floor(a)');
+console.log(Math.floor(a));
+
+console.log('Math.ceil(a)');
+console.log(Math.ceil(a));
+
+console.log('Math.round(a)');
+console.log(Math.round(a));
+
+console.log('Math.PI');
+console.log(Math.PI.toFixed(48));
+
+console.log('isNaN("str"/2)');
+console.log(isNaN("str"/2));
+
+console.log('1/0');
+console.log(1/0);
+
+console.log('-1/0');
+console.log(-1/0);
